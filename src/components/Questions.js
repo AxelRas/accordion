4ed11fa354f7406a2181bb8208493174a10a8d78
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-27 20:31:03 -0300

    Expand button function

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
+function showAnswer(id, answer) {
 
+    let content = $("#button-" + id).text();
+
+    if(content === "+"){
+        $("#question-container-" + id).append("<p id='answer-"+ id +"'>"+ answer +"</p>");
+        $("#button-"+ id).empty();
+        $("#button-"+ id).text("-");
+    } else {
+        $("#answer-" + id).remove();
+        $("#button-"+ id).empty();
+        $("#button-"+ id).text("+");
+    }
+}
 
 export default class Questions extends Component {
     render() {
         return (
             <div className="questions">
                 {this.props.questions.map(question => (
-                    <div className="question-container">
+                    <div id={"question-container-" + question.id} key={"key" + question.id} className="question-container">
                         <div className="question">
-                            <p className="question">{question.q}</p>
-                            <button onClick={}>+</button>
+                            <p className="question-text">{question.q}</p>
+                            <button id={"button-" + question.id} onClick={() => {showAnswer(question.id, question.a)}}>+</button>
                         </div>
                         
                     </div>
