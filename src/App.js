c44c9418f4a1141937344ca3bc3e37f43df5da30
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-27 18:21:10 -0300

    Basic format, no function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Questions from './components/Questions';
+import data from './questions.json'
 
 class App extends React.Component {
   constructor() {
@@ -9,7 +10,12 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="container">
+          <div className="title">
+            <h1>Serious questions</h1>
+          </div>
+          <Questions questions={data.questions}/>
+        </div>
       </div>
     );
   }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+
 
 export default class Questions extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="questions">
+                {this.props.questions.map(question => (
+                    <div className="question-container">
+                        <div className="question">
+                            <p className="question">{question.q}</p>
+                            <button onClick={}>+</button>
+                        </div>
+                        
+                    </div>
+                ))}
             </div>
         )
     }
